07ba4baccbcc7f2638a0535dc7467e50949841f4
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2024-05-06 15:05:58 +0100

    node_ids parenteses paranteses

--- RAFT/Candidate.py
+++ RAFT/Candidate.py
@@ -9,8 +9,8 @@ class Candidate(SharedState):
         # Set State
         super().changeState(sharedState)
         # Candidate 
-        self.voters = {node_id}
-        self.votedFor = node_id
+        self.voters = {node_id()}
+        self.votedFor = node_id()
         self.currentTerm += 1
         self.requestVote()
         self.timer.create(lambda: send(node_id(), type="startElection"))
@@ -18,14 +18,14 @@ class Candidate(SharedState):
 
     def requestVote(self):
         lenLog = len(self.log)
-        for dest_id in self.node_ids:
+        for dest_id in self.node_ids():
             send(dest_id, type="request_vote", term=self.currentTerm, lastLogIndex = lenLog, lastLogTerm = self.log[-1] if lenLog==0 else -1)
     
     def handleVote(self, msg):
         if msg.body.term == self.currentTerm:
             self.voters.add(msg.src)
 
-            if len(self.voters) >= len(node_ids) / 2: # case (a): a Candidate received majority of votes
+            if len(self.voters) >= len(node_ids()) / 2: # case (a): a Candidate received majority of votes
                 setActiveClass(Leader())
     
     def becomeFollower():
